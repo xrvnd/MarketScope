94b872c52f1ae33c4947d11a45983eb32defa78f
xrvnd <emmaneni.arvind@gmail.com>
2025-05-16 13:14:19 +0530

    Adding firebase config with API keys

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAKBt4t49s-0_XjFgruaAhDQjLfJqlP4To",
-    authDomain: "market-scope-a50ef.firebaseapp.com",
-    projectId: "market-scope-a50ef",
-    storageBucket: "market-scope-a50ef.firebasestorage.app",
-    messagingSenderId: "543956486116",
-    appId: "1:543956486116:web:27522853ac25215f77a9f1",
-    measurementId: "G-RKYR27JFE1"
-  };
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
