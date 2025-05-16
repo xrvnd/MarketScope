800c993b8b99a0dfb86f41f35fdbd97cd04a1a46
xrvnd <emmaneni.arvind@gmail.com>
2025-05-16 13:00:44 +0530

    adding firebase key

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,11 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "YOUR_DOMAIN",
-//   projectId: "YOUR_PROJECT_ID",
-//   appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyAKBt4t49s-0_XjFgruaAhDQjLfJqlP4To",
+    authDomain: "market-scope-a50ef.firebaseapp.com",
+    projectId: "market-scope-a50ef",
+    storageBucket: "market-scope-a50ef.firebasestorage.app",
+    messagingSenderId: "543956486116",
+    appId: "1:543956486116:web:27522853ac25215f77a9f1",
+    measurementId: "G-RKYR27JFE1"
+  };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
